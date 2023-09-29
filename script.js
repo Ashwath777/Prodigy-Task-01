8f74e769e37099ea16c6e99278e33158868d59bb
anubhav7747 <anubhavtiwari007005@gmail.com>
2023-09-29 12:22:58 +0530

    added script

---
+++ script.js
@@ -0,0 +1,36 @@
+function hover(x) {
+    x.style.color = "red";
+    x.style.backgroundColor = "#74564c";
+    x.style.textDecoration = "underline";
+}
+
+function noHover(x) {
+    x.style.color = "#955d19";
+    x.style.backgroundColor = "transparent";
+    x.style.textDecoration = "none";
+}
+
+function myHome(x) {
+    location.href = "#home"
+}
+
+function myAbout(x) {
+    location.href = '#about';
+}
+
+function myServices(x) {
+    location.href = '#services';
+}
+
+function myContact(x) {
+    location.href = "#contact";
+}
+
+function mailOver(x) {
+    x.style.textDecoration = "underline";
+}
+
+function mailOut(x) {
+    x.style.textDecoration = "none";
+    x.style.color = "000";
+}
